1c08f799ddaf3e34ebce20028404147ffaf7b91c
hui <holasunhui@sina.com>
2022-05-03 01:11:35 +1000

    master merge commit

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hot-fix merge branch");
+        System.out.println("master merge branch");
     }
 }
