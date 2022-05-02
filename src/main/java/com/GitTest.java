193f1c134a7421101c1b833a11deaeeb4cf5729c
huisina <104730674+huisina@users.noreply.github.com>
2022-05-03 02:06:11 +1000

    web change...

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("master merge branch");
         System.out.println("master merge branch");
         System.out.println("hotfix merge branch");
+        System.out.println("change in web");
     }
 }
