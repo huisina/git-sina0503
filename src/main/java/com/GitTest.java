7480cc1e44e12be9a833d723285c85c60ffcd45d
hui <holasunhui@sina.com>
2022-05-03 01:15:39 +1000

    hot...

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
-        System.out.println("hot-fix merge branch");
+        System.out.println("hotfix merge branch");
     }
 }
