c7090718a1ba27b82876c3d6747a60e3befc881b
hui <holasunhui@sina.com>
2022-05-03 01:13:57 +1000

    master....

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -8,7 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
-        System.out.println("hot-fix merge branch");
+        System.out.println("master merge branch");
         System.out.println("master merge branch");
     }
 }
